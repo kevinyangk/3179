928e8b95d3ba1b6e2ab3af316920f1bfd51f5547
kevinyangk <115760070+kevinyangk@users.noreply.github.com>
2022-10-24 19:54:28 +0800

    Add files via upload

---
+++ ass2/vega_lite_vis.js
@@ -0,0 +1,15 @@
+var vg_1 = "year.json";
+vegaEmbed("#line_chart", vg_1).then(function(result) {
+}).catch(console.error);
+var vg_2 = "map.json";
+vegaEmbed("#map", vg_2).then(function(result) {
+}).catch(console.error);
+var vg_3 = "type.json";
+vegaEmbed("#bar_chart", vg_3).then(function(result) {
+}).catch(console.error);
+var vg_4 = "reason.json";
+vegaEmbed("#reason_chart", vg_4).then(function(result) {
+}).catch(console.error);
+var vg_5 = "takeof.json";
+vegaEmbed("#take_chart", vg_5).then(function(result) {
+}).catch(console.error);
